b9e6da84a31b875d6aa2df0268e55cb7763be53e
SeptMeteor <3466512920@qq.com>
2022-07-08 17:33:25 +0800

    TEST

--- src/main/java/com/example/klein/KleinApplication.java
+++ src/main/java/com/example/klein/KleinApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KleinApplication {
 
 	public static void main(String[] args) {
+		int a;
 		SpringApplication.run(KleinApplication.class, args);
 	}
 
